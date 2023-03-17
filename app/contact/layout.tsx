5cf3f9dc966b955e26c151ac3af9e6965219b3b1
alekspanteli <alekspanteli@rambler.ru>
2023-03-17 07:10:00 +0000

    add contact layout

---
+++ app/contact/layout.tsx
@@ -0,0 +1,9 @@
+export default function ContactLayout({children}: 
+    {children: React.ReactNode;}) {
+  return (
+    <div>
+        <div>contact layout</div>
+        {children}
+    </div>
+  );
+}
